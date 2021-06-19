af247ad0a32b0dc9da6ef99c03a1cf02294d26e7
samuelbelolo <samuel.belolo@hetic.net>
2021-06-19 22:28:17 +0200

    :fire: add consumer

---
+++ worker/consumer.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+const amqplib = require("amqplib/callback_api");
+const { Pool } = require("pg");
+const sharp = require("sharp");
+const { promisify } = require("util");
+// redis client
+const client = require("redis").createClient(process.env.REDIS_URL);
+const amqp_url = process.env.AMQP_URL;
+const get = promisify(client.get).bind(client);
+const del = promisify(client.del).bind(client);
+
+const pool = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+const transform = async (data) => {
+  const imgBuffer = Buffer.from(data, "base64");
+  return await sharp(imgBuffer).resize(32, 32).toBuffer();
+};
+
+const updateDb = (id, data) => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      "UPDATE users SET avatar = $1 WHERE id = $2",
+      [data, id],
+      (err, res) => {
+        if (err) reject(err);
+        resolve();
+      }
+    );
+  });
+};
+
+const resizeImage = async (id) => {
+  let image = await get(`user_${id}_avatar`);
+  console.log("[x] retrieve item from redis cache");
+  if (!image) return;
+  image = image.split(";base64,");
+  const mime = image[0];
+  const data = image[1];
+
+  let cropedImage = await transform(data);
+  console.log("[x] reduced size transformation done");
+  croppedImage = `${mime};base64,${cropedImage.toString("base64")}`;
+  await updateDb(id, croppedImage);
+  console.log("[x] user " + id + " updated");
+  await del(`user_${id}_avatar`);
+  console.log("[x] original image deleted");
+};
+
+const bail = (err) => {
+  console.error(err);
+  process.exit(1);
+};
+
+// Consumer
+function consumer(conn) {
+  conn.createChannel(on_open);
+  function on_open(err, ch) {
+    if (err != null) bail(err);
+    ch.assertQueue("avatar_tasks");
+    ch.consume("avatar_tasks", async (msg) => {
+      if (msg !== null) {
+        const key = msg.content.toString();
+        console.log("[x] receive key : " + key);
+        await resizeImage(key);
+        await ch.ack(msg);
+      }
+    });
+  }
+}
+
+amqplib.connect(amqp_url, function (err, conn) {
+  if (err != null) bail(err);
+  consumer(conn);
+});
