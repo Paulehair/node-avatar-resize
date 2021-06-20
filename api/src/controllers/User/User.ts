bfda2b7c9ecaf2cfc6bb859ec9d9dfb33c536e51
theofenique <theo@braineet.com>
2021-06-20 14:19:38 +0200

    feat(core): wip with route POST /user

--- api/src/controllers/User/User.ts
+++ api/src/controllers/User/User.ts
@@ -1,9 +1,26 @@
 import { Request, Response } from 'express';
 import { CrudController } from '../CrudController';
+import redis from 'redis'
+
+import {User} from '../../models/User'
 
 export class UserController extends CrudController {
   public create(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): void {
-    throw new Error("Method not implemented.");
+    console.log(req.body)
+    const newUser: User = {
+      id: req.body.id,
+      username: "string",
+      password: "string",
+      email: "string"
+    }
+
+    const client = redis.createClient({
+      host: 'redis-server',
+      port: 6379,
+      password: 'pwd-redis'
+    });
+
+    res.json(newUser)
   }
 
   public read(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): void {
