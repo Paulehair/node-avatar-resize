e2089dc3abee10f72f0a30753e5095d7c15824bc
Aymeric Moehn <aymeric.moehn@gmail.com>
2021-06-22 10:54:07 +0200

    update(core): Fix queue not working propperly

--- api/src/controllers/User/User.ts
+++ api/src/controllers/User/User.ts
@@ -4,24 +4,23 @@ import multer from 'multer'
 import { Request, Response } from 'express';
 
 import { CrudController } from '../CrudController';
-import { User } from '../../models/User'
+import { User } from '../../models/User';
 
-import * as redisUtils from '../../middlewares/redis'
+import * as redisUtils from '../../middlewares/redis';
+import * as mqUtils from '../../middlewares/queue';
 import * as queries from './queries';
 
 const upload = multer({
-  dest:"./temp"
+  dest: "./temp"
 })
 
 export class UserController extends CrudController {
-  public create(req:any, res: Response): void {
+  public create(req: any, res: Response): void {
     const filepath = path.normalize(req.file.path)
-    const errors:[string?] = []
+    const errors: [string?] = []
 
-    fs.readFile(filepath, (fileErr:any, fileData:any) => {
+    fs.readFile(filepath, { encoding: 'base64' }, (fileErr: any, imgData: string) => {
       if (fileErr) errors.push("Error reading file")
-
-      const imgData = Buffer.from(fileData).toString('base64')
 
       const newUser: User = {
         username: req.body.username,
@@ -30,23 +29,27 @@ export class UserController extends CrudController {
       }
 
       queries.insertUser(newUser)
-        .then((dbRes:any) => {
+        .then((dbRes: any) => {
           const userID = dbRes.rows[0].id
 
-          redisUtils.setImageInCache(`tempimg_${userID}`, imgData).then(() => {
-            fs.unlink(filepath, (unlinkErr:any) => {
-              if (unlinkErr) errors.push("Cannot remove file from temp folder")
+          mqUtils.addImageToQueue(userID, imgData)
+
+          redisUtils.setImageInCache(`tempimg_${userID}`, imgData)
+            .then(() => {
+              fs.unlink(filepath, (unlinkErr: any) => {
+                if (unlinkErr) errors.push("Cannot remove file from temp folder")
+              })
             })
-          })
-          .catch((err:string) => {
-            errors.push(err)
-          })
+            .catch((err: string) => {
+              errors.push(err)
+            })
+
 
           res.json({
             errors: errors,
-            data: newUser
+            data: { ...newUser, avatar: imgData }
           })
-        }).catch((err:string) => {
+        }).catch((err: string) => {
           errors.push(err)
           res.json({
             errors: errors,
@@ -57,10 +60,10 @@ export class UserController extends CrudController {
   }
 
   public read(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): void {
-    const userID:any = req.query.id
-    const errors:[string?] = []
+    const userID: any = req.query.id
+    const errors: [string?] = []
 
-    const makeResponse = (user:Object, errors:[string?], res:Response) => {
+    const makeResponse = (user: Object, errors: [string?], res: Response) => {
       res.json({
         errors: errors,
         data: user
@@ -68,15 +71,14 @@ export class UserController extends CrudController {
     }
 
     if (userID) {
-      queries.getUser(userID).then((user:any) => {
-        console.log(user)
+      queries.getUser(userID).then((user: any) => {
         if (user[0].avatar == null) {
           redisUtils.getImageFromCache(`tempimg_${userID}`)
-            .then((img:string) => {
+            .then((img: string) => {
               user[0].avatar = img
               makeResponse(user[0], errors, res)
             })
-            .catch((err:string) => {
+            .catch((err: string) => {
               errors.push(err)
               makeResponse({}, errors, res)
             })
