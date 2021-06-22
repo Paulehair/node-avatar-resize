3c927346abe841adb33418f7bc2bc9353f3f8326
theofenique <theo@braineet.com>
2021-06-22 10:56:16 +0200

    feat(fix): Fixing error handling

--- api/src/controllers/User/User.ts
+++ api/src/controllers/User/User.ts
@@ -1,6 +1,5 @@
 import fs from 'fs'
 import path from 'path'
-import multer from 'multer'
 import { Request, Response } from 'express';
 
 import { CrudController } from '../CrudController';
@@ -9,17 +8,20 @@ import { User } from '../../models/User'
 import * as redisUtils from '../../middlewares/redis'
 import * as queries from './queries';
 
-const upload = multer({
-  dest:"./temp"
-})
+
+const makeUserResponse = (user:Object, error:string, res:Response) => {
+  res.json({
+    error: error,
+    data: user
+  });
+}
 
 export class UserController extends CrudController {
   public create(req:any, res: Response): void {
     const filepath = path.normalize(req.file.path)
-    const errors:[string?] = []
 
-    fs.readFile(filepath, (fileErr:any, fileData:any) => {
-      if (fileErr) errors.push("Error reading file")
+    fs.readFile(filepath, (readFileErr:any, fileData:any) => {
+      if (readFileErr) makeUserResponse(null, "Error reading file: "+readFileErr, res)
 
       const imgData = Buffer.from(fileData).toString('base64')
 
@@ -33,60 +35,43 @@ export class UserController extends CrudController {
         .then((dbRes:any) => {
           const userID = dbRes.rows[0].id
 
-          redisUtils.setImageInCache(`tempimg_${userID}`, imgData).then(() => {
-            fs.unlink(filepath, (unlinkErr:any) => {
-              if (unlinkErr) errors.push("Cannot remove file from temp folder")
+          redisUtils.setImageInCache(`tempimg_${userID}`, imgData)
+            .then(() => {
+              fs.unlink(filepath, (unlinkErr:any) => {
+                if (unlinkErr) makeUserResponse(null, "Error removing temp file: "+unlinkErr, res)
+              })
+            }).catch((cacheErr:string) => {
+              makeUserResponse(null, "Error caching avatar: "+cacheErr, res)
             })
-          })
-          .catch((err:string) => {
-            errors.push(err)
-          })
-
-          res.json({
-            errors: errors,
-            data: newUser
-          })
+            
+          newUser.id = userID
+          makeUserResponse(newUser, null, res)
         }).catch((err:string) => {
-          errors.push(err)
-          res.json({
-            errors: errors,
-            data: {}
-          })
+          makeUserResponse(null, err, res)
         })
     })
   }
 
   public read(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): void {
     const userID:any = req.query.id
-    const errors:[string?] = []
-
-    const makeResponse = (user:Object, errors:[string?], res:Response) => {
-      res.json({
-        errors: errors,
-        data: user
-      });
-    }
 
     if (userID) {
       queries.getUser(userID).then((user:any) => {
-        console.log(user)
         if (user[0].avatar == null) {
           redisUtils.getImageFromCache(`tempimg_${userID}`)
             .then((img:string) => {
               user[0].avatar = img
-              makeResponse(user[0], errors, res)
+              makeUserResponse(user[0], null, res)
             })
-            .catch((err:string) => {
-              errors.push(err)
-              makeResponse({}, errors, res)
+            .catch((cacheErr:string) => {
+              makeUserResponse({}, "Error getting avatar from cache: "+cacheErr, res)
             })
         } else {
-          makeResponse(user[0], errors, res)
+          makeUserResponse(user[0], null, res)
         }
       })
     } else {
-      errors.push('No ID provided.')
-      makeResponse({}, errors, res)
+      makeUserResponse({}, "No ID provided.", res)
     }
   }
 
